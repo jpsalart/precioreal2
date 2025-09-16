cd717f1feb8596c9bf4ff105e9a394122cbee77a
jpsalart <142902377+jpsalart@users.noreply.github.com>
2025-09-17 01:05:33 +0200

    Update kv-debug.js

--- api/kv-debug.js
+++ api/kv-debug.js
@@ -1,7 +1,10 @@
 export default function handler(_req, res) {
   res.json({
     hasUrl: !!process.env.KV_REST_API_URL,
-    hasToken: !!process.env.KV_REST_API_TOKEN
+    hasToken: !!process.env.KV_REST_API_TOKEN,
+    vercelEnv: process.env.VERCEL_ENV,
+    deployment: process.env.VERCEL_URL
   });
 }
+
 
